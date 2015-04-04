5745b9ac1eaf49789054ea5880f2f8956ee8344b
callum white <callum.white@auspost.com.au>
2015-04-04 20:30:11 +1100

    Preliminary ubuntu setup. Testing now...

---
+++ ubuntu-setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Parameters
+#1: name for new user
+#2: public ssh key of local machine (use ssh_keygen)
+
+
+#ssh root@SERVER_IP_ADDRESS
+
+# Environment setup
+echo "Creating user $1..."
+adduser $1
+gpasswd -a $1 sudo
+
+## SSH key setup
+echo "Changing to user $1..."
+su - $1
+mkdir .ssh
+chmod 700 .ssh
+cd .ssh
+echo $2 > authorized_keys
+chmod 600 authorized_keys
+exit #return to root user
+## Other options for /etc/ssh/sshd_config:
+### Change port number: Port 4444
+### PermitRootLogin: no
+echo "Restarting ssh to use new settings..."
+service ssh restart
+
+
+# Install stuff
+echo "Installing stuff with user $1..."
+su - $1
+## Ruby
+echo "Installing Ruby..."
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3
+rvm rubygems current
+sudo apt-get install nodejs #for javascript runtime environment
+gem install bundle
